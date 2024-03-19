46674baa817bc9ac3e143e943686aa1104f89a34
vincenttommi <vincenttommi@gmail.com>
2024-03-19 21:26:54 +0300

    adding rest authentication

--- piggbanks/piggbanks/settings.py
+++ piggbanks/piggbanks/settings.py
@@ -128,7 +128,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK  = {
+    
+    "DEFAULT_AUTHENTICATION_CLASSES":[
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        
+    ],
+    
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':100
 }
 #allows  me  to modify how large results are split into individual  pages of data
+
