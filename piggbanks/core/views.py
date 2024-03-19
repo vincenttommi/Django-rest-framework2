ccf472d7239316c197024c785e3d57833f4b35f2
vincenttommi <vincenttommi@gmail.com>
2024-03-19 12:46:20 +0300

    adding pagination in serializers

--- piggbanks/core/views.py
+++ piggbanks/core/views.py
@@ -3,18 +3,22 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from .models import Currency, Category, Transaction
+from rest_framework.pagination import PageNumberPagination  
 from .serializers import CategorySerializer, CurrencySerializer, ReadTransactionSerializer,  WriteTransactionSerializer
 
 class CurrencyListAPIView(ListAPIView):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
+    pagination_class  = None
 
 class CategoryModelViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
 
 class TransactionModelViewSet(ModelViewSet):
     queryset = Transaction.objects.all()
+   
     
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
--- piggbanks/piggbanks/settings.py
+++ piggbanks/piggbanks/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK  = {
-    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':100
 }
 #allows  me  to modify how large results are split into individual  pages of data
